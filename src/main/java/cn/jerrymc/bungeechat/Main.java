6208a04c632d78ea470a187f092d7c3069cb3fc9
CodeZhangBorui <61909157+CodeZhangBorui@users.noreply.github.com>
2023-01-07 05:18:18 +0000

    add msg command to test

--- src/main/java/cn/jerrymc/bungeechat/Main.java
+++ src/main/java/cn/jerrymc/bungeechat/Main.java
@@ -5,25 +5,26 @@ import net.md_5.bungee.api.plugin.Plugin;
 public final class Main extends Plugin {
     @Override
     public void onEnable() {
-        getLogger().info("§c ____                               ____ _           _   ");
-        getLogger().info("§c| __ ) _   _ _ __   __ _  ___  ___ / ___| |__   __ _| |_ ");
-        getLogger().info("§c|  _ \\| | | | '_ \\ / _` |/ _ \\/ _ \\ |   | '_ \\ / _` | __|");
-        getLogger().info("§c| |_) | |_| | | | | (_| |  __/  __/ |___| | | | (_| | |_ ");
-        getLogger().info("§c|____/ \\__,_|_| |_|\\__, |\\___|\\___|\\____|_| |_|\\__,_|\\__|");
-        getLogger().info("§c                   |___/                                 ");
-        getLogger().info("Plugin by §bbcmray & CodeZhangBorui§r, version §b1.3§r, Loading...");
+        getLogger().info("§c ____                               ____ _           _   §r");
+        getLogger().info("§c| __ ) _   _ _ __   __ _  ___  ___ / ___| |__   __ _| |_ §r");
+        getLogger().info("§c|  _ \\| | | | '_ \\ / _` |/ _ \\/ _ \\ |   | '_ \\ / _` | __|§r");
+        getLogger().info("§c| |_) | |_| | | | | (_| |  __/  __/ |___| | | | (_| | |_ §r");
+        getLogger().info("§c|____/ \\__,_|_| |_|\\__, |\\___|\\___|\\____|_| |_|\\__,_|\\__|§r");
+        getLogger().info("§c                   |___/                                 §r");
+        getLogger().info("Plugin by §bbcmray & CodeZhangBorui§r, version §b${project.version}§r, §lLoading...§r");
         // 注册监听器
         getProxy().getPluginManager().registerListener(this, new PostLoginEvent());
         getProxy().getPluginManager().registerListener(this, new ChatEvent(this));
 
         // 注册指令
-//        getProxy().getPluginManager().registerCommand(this,new AnnounceCommand(this));
-        getLogger().info("§c[BungeeChat]§r 插件已加载!");
+        getProxy().getPluginManager().registerCommand(this,new MsgCommand(this));
+
+        getLogger().info("§c§l[BungeeChat]§r 插件已加载!");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-        getLogger().info("§c[BungeeChat]§r 插件已卸载!");
+        getLogger().info("§c§l[BungeeChat]§r 插件已卸载!");
     }
 }
---
+++ src/main/java/cn/jerrymc/bungeechat/MsgCommand.java
@@ -0,0 +1,52 @@
+package cn.jerrymc.bungeechat;
+
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.plugin.Command;
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class MsgCommand extends Command {
+    private final Plugin plugin;
+
+    public MsgCommand(Plugin plugin){
+        super("msg");
+        this.plugin = plugin;
+    }
+
+    @Override
+    public void execute(CommandSender sender, String[] args) {
+        String displayPrefix = "[" + sender.getName() + " -> " + args[0] + "] "
+
+        StringBuilder displayMsg = new StringBuilder();
+        for (int i = 1; i < args.length; i++) {
+            msg.append(args[i]).append(" ");
+        }
+
+        TextComponent sendPrefix = new TextComponent(displayPrefix);
+        fromPlayer.setColor(ChatColor.WHITE);
+        fromPlayer.setBold(false);
+        TextComponent messageMain = new TextComponent(displayMsg);
+        toPlayer.setColor(ChatColor.WHITE);
+        toPlayer.setBold(false);
+
+        sendPrefix.addExtra(messageMain);
+
+        boolean successful = false;
+
+        for(ProxiedPlayer recPlayer:plugin.getProxy().getPlayers()) {
+            if(recPlayer.getName() == args[0]) {
+                recPlayer.sendMessage(sendPrefix);
+                successful = true;
+            }
+        }
+
+        if(successful == true) {
+            sender.sendMessage(sendPrefix);
+            plugin.getLogger.info(sendPrefix);
+        } else {
+            sender.sendMessage("§c§l错误：§r玩家§l" + args[0] + "§r不存在！");
+        }
+    }
+}
