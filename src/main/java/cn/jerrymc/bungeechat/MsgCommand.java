7266806e38e101ce6c0f2911b62f7ddf8f0d914a
CodeZhangBorui <61909157+CodeZhangBorui@users.noreply.github.com>
2023-01-07 06:01:08 +0000

    import ArrayList lib

--- src/main/java/cn/jerrymc/bungeechat/MsgCommand.java
+++ src/main/java/cn/jerrymc/bungeechat/MsgCommand.java
@@ -9,6 +9,7 @@ import net.md_5.bungee.api.plugin.TabExecutor;
 import net.md_5.bungee.api.plugin.Plugin;
 
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
