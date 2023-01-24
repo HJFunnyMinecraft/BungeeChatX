49c04ed95a3e5856b5e68a005b5bf2047f237c72
CodeZhangBorui <61909157+CodeZhangBorui@users.noreply.github.com>
2023-01-24 10:37:14 +0000

    init JoinedPlayers with ArrayList

--- src/main/java/site/hjfunny/bungeechatx/ProxiedPlayerList.java
+++ src/main/java/site/hjfunny/bungeechatx/ProxiedPlayerList.java
@@ -1,8 +1,10 @@
 package site.hjfunny.bungeechatx;
 
 import net.md_5.bungee.api.connection.ProxiedPlayer;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProxiedPlayerList {
-    static public List<ProxiedPlayer> JoinedPlayers;
+    static public List<ProxiedPlayer> JoinedPlayers = new ArrayList<ProxiedPlayer>();
 }
