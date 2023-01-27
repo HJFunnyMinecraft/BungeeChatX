66d8e4e6d1186cae8a2bde88f78d1299027116a1
CodeZhangBorui <61909157+CodeZhangBorui@users.noreply.github.com>
2023-01-27 09:25:40 +0000

    fix  CVE-2022-25845

--- src/main/java/site/hjfunny/bungeechatx/Main.java
+++ src/main/java/site/hjfunny/bungeechatx/Main.java
@@ -24,7 +24,7 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson2.JSON;
 
 public final class Main extends Plugin {
 
